2e976560683ce6b3da6f77c539a7942cc7b8581d
Pietrangelo Masala <posta@pietrangelo.net>
2020-03-26 11:22:25 +0100

    CRD creation fixed

--- start-oc.sh
+++ start-oc.sh
@@ -18,9 +18,9 @@ oc new-project my-app
 
 # Clone the entando custom resourses and install them 
 git clone https://github.com/entando-k8s/entando-k8s-custom-model.git
-pushd entando-k8s-custom-model/src/main/resources/crd
-oc create - f .
-popd
+oc create -f entando-k8s-custom-model/src/main/resources/crd/
+
+echo "----------------------------------------------------------------"
 
 myIP=$(ifconfig eth1 | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p')
 echo "Custom resource definition installed."
